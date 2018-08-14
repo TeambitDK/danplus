482a21128fe72bd586a65e76c903337ba2c2e44d
Olliver Stensbøl <olliver@teambit.dk>
2018-08-14 13:13:36 +0200

    changes to server.js

--- server.js
+++ server.js
@@ -1,52 +1,17 @@
-const app = require("./backend/app");
-const debug = require("debug")("node-angular");
-const http = require("http");
+const express= require('express'),
+	 path = require('path');
 
-const normalizePort = val => {
-  var port = parseInt(val, 10);
+const app =express();
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+app.use(express.static('./dist/website'));
 
-  return false;
-};
+app.get('/*', (req,res)=>{
 
-const onError = error => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
+res.sendFile(path.join(__dirname,'/dist/website/index.html'));
 
-const onListening = () => {
-  const addr = server.address();
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-  debug("Listening on " + bind);
-};
+});
 
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
-
-const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
-server.listen(port);
+app.listen(process.env.PORT || 8080, ()=>{
+console.log('Server started');
+})
